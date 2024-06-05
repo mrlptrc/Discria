732ec8dda8b9fd882e6ce38ef37d313254276a40
mrlptrc <murilopatricio@gmail.com>
2024-06-05 18:32:11 -0300

    feat: geração spring

---
+++ src/main/java/com/discord/discria/DiscriaApplication.java
@@ -0,0 +1,13 @@
+package com.discord.discria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discord/discria/DiscriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discord.discria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
