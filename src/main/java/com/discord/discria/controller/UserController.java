c597818e8f856a59ccd035c5cee99bd1ecd44d7a
mrlptrc <murilopatricio@gmail.com>
2024-06-06 15:16:29 -0300

    feat: user service + controller pendente + authcontroller feito/comecando spring security

--- src/main/java/com/discord/discria/controller/UserController.java
+++ src/main/java/com/discord/discria/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.discord.discria.controller;
 
+import com.discord.discria.dto.UserDTO;
 import com.discord.discria.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> registerUser(@RequestBody UserDTO userDTO){
+        return ResponseEntity.ok(userService.registerUser(userDTO));
+    }
 }
--- src/main/java/com/discord/discria/service/UserService.java
+++ src/main/java/com/discord/discria/service/UserService.java
@@ -1,5 +1,37 @@
 package com.discord.discria.service;
 
+import com.discord.discria.dto.UserDTO;
+import com.discord.discria.entity.User;
+import com.discord.discria.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //GET:  registra um usuario
+    public UserDTO registerUser(UserDTO userDTO){
+        User user = new User();
+        user.setUsername(userDTO.getUsername());
+        user.setPassword(userDTO.getPassword());
+        user.setEmail(userDTO.getEmail());
+        user.setId(userDTO.getId());
+        User savedUser = userRepository.save(user);
+        return mapToDTO(savedUser);
+    }
+
+
+    //GET:  cria objeto e popula ele com os dados para serem enviados pro banco de dados
+    public UserDTO mapToDTO(User user){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setUsername(user.getUsername());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setEmail(user.getEmail());
+        return userDTO;
+    }
 
 }
